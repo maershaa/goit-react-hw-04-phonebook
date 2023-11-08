4aeca4b6ccb6d9a993b910d7cfb17b34e3a9905e
maershaa <valeria.work2022@gmail.com>
2023-11-08 23:34:35 +0200

    update
    goit-react-hw-04-phonebook
    при перезагрузке страницы удаляются данные из localStorage и фильтрация не работает

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,105 +1,91 @@
-import React, { Component } from "react";
-import ContactForm from "./ContactForm/ContactForm"; // Импортируем компонент формы для добавления контактов
-import ContactList from  "./Contacts/ContactsList"; // Импортируем компонент списка контактов
-import Filter from "./Filter/Filter";
+import React, { useState, useEffect } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './Contacts/ContactsList';
+import Filter from './Filter/Filter';
 import css from './App.module.css';
 
+const App = () => {
+  const [contacts, setContacts] = useState([]); // Состояние для хранения контактов
+  const [filter, setFilter] = useState(''); // Состояние для хранения значения фильтрации
 
-class App extends Component {
-state = {
-  contacts: [],
-  filter: ''
-}
-  
-  
-componentDidMount() {
-  // Метод componentDidMount вызывается после того, как компонент был добавлен в DOM.
-  // Попытка получить данные из локального хранилища браузера.
-  const stringifiedContacts = localStorage.getItem('contacts');
-  // Если данные были найдены в локальном хранилище, попытаемся распарсить их.
-  // Если данные не найдены (null), установим начальное значение из contactsData.
-  const parsedContacts = JSON.parse(stringifiedContacts) || [];
-  // Устанавливаем состояние компонента на основе данных из локального хранилища или начальных данных.
-  this.setState({ contacts: parsedContacts });
-}
+  // Загрузка данных из localStorage при монтировании компонента
+  useEffect(() => {
+    const stringifiedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(stringifiedContacts) || [];
+    setContacts(parsedContacts);
+  }, []);
 
-  
-componentDidUpdate(_, prevState) {
-  // Метод componentDidUpdate вызывается после обновления компонента.
-  // В данном случае, мы сравниваем предыдущие контакты (из prevState) с текущими контактами (из this.state).
-  if (prevState.contacts !== this.state.contacts) {
-    // Если предыдущие контакты не равны текущим контактам, это означает, что произошло изменение контактов.
-    // Мы преобразуем массив контактов в формат JSON и сохраняем его в локальное хранилище браузера.
-    const stringifiedContacts = JSON.stringify(this.state.contacts);
+  // Сохранение данных в localStorage при изменении контактов
+  useEffect(() => {
+    const stringifiedContacts = JSON.stringify(contacts);
     localStorage.setItem('contacts', stringifiedContacts);
-  }
-}
+  }, [contacts]);
 
-  
-addContact = newContact => {
-  // Приводим имя нового контакта к нижнему регистру
-  const normalizeName = newContact.name.toLowerCase();
+  // Функция для добавления нового контакта
+  const addContact = newContact => {
+    // Проверка на дубликат контакта
+    const normalizeName = newContact.name.toLowerCase();
+    const isDuplicate = contacts.some(
+      contact => contact.name.toLowerCase() === normalizeName
+    );
 
-  // Проверяем, существует ли контакт с таким же именем (нормализованным) среди текущих контактов
-  const isDuplicate = this.state.contacts.some(
-    contact => contact.name.toLowerCase() === normalizeName
-  );
+    if (isDuplicate) {
+      alert(`${newContact.name} уже есть в контактах`);
+      return;
+    }
 
-  // Если контакт уже существует, выводим сообщение и завершаем выполнение функции
-  if (isDuplicate) {
-    alert(`${newContact.name} уже есть в контактах`);
-    return;
-  }
+    // Обновление состояния, добавляя новый контакт
+    setContacts(prevContacts => [...prevContacts, newContact]); // Используем функциональное обновление вместо:
+    //   this.setState(prevState => ({
+    //     contacts: [...prevState.contacts, newContact],
+    //   }));
+    // };
+  };
 
-  // Если контакт не является дубликатом, обновляем состояние, добавляя новый контакт в массив контактов
-  this.setState(prevState => ({
-    contacts: [...prevState.contacts, newContact],
-  }));
-};
+  // Функция для удаления контакта по его ID
+  const deleteContact = contactId => {
+    // Обновление состояния, фильтруя контакты и удаляя контакт с заданным ID
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+  // Вместо этого
+  // const deleteContact = contactId => {
+  // setState(prevState => ({
+  // contacts: prevState.contacts.filter(
+  //   contact => contact.id !== contactId
+  // ),}));
+  // };
 
-// Метод для удаления контакта по его ID
-deleteContact = (contactId) => {
-  this.setState(prevState => ({
-    // Обновляем состояние, фильтруя контакты и оставляя только те, у которых ID не совпадает с заданным contactId
-    contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-  }));
-};
-
-    // Метод для получения отфильтрованного массива контактов
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  // Функция для фильтрации контактов
+  const getFilteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-
   // Обработчик изменения значения фильтра
-  handleFilterChange = (filter) => {
-    this.setState({ filter });
-    console.log(filter)
-  }
-  render() {
+  const handleFilterChange = newFilterValue => {
+    // Обновление состояния фильтра
+    setFilter(newFilterValue);
+  };
 
-        const filteredContacts = this.getFilteredContacts(); // Получаем отфильтрованный массив контактов
+  // Получение отфильтрованного массива контактов
+  // const filteredContacts = getFilteredContacts();
 
-    return (
-      <div className={css.container}>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-         {/* Рендерим форму для добавления контактов и передаем метод addContact как обработчик */}
-        
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.handleFilterChange} />
-
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />{' '}
+      {/* Рендеринг формы для добавления контакта и передача метода addContact как обработчика */}
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
       <ContactList
-          contacts={filteredContacts} // Передаем отфильтрованный массив контактов
-            onDeleteContact={this.deleteContact} 
-        />
-      </div>
-    );
-  }
-}
+        contacts={getFilteredContacts()} // Передача отфильтрованного массива контактов
+        onDeleteContact={contactId => deleteContact(contactId)} // Передача функции для удаления контакта с аргументом
+      />
+    </div>
+  );
+};
 
 export default App;
-
